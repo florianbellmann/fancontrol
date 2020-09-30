d95b42194330af14f3d3cfebca671fcde2bcc888
florianjuesten <flojuesten@gmail.com>
2020-09-30 18:03:26 +0200

    added final service status check

--- install_service.sh
+++ install_service.sh
@@ -5,3 +5,5 @@ sudo chmod 644 /lib/systemd/system/fancontrol.service
 sudo systemctl daemon-reload
 sudo systemctl enable fancontrol.service
 sudo systemctl start fancontrol.service
+
+sudo systemctl status fancontrol.service

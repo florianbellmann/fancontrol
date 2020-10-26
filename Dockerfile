0706e2629e988a5ba34d6ff4154c5185e40afbe4
florianjuesten <flojuesten@gmail.com>
2020-10-26 18:05:27 +0100

    fixed web interface

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:14
 
-COPY index.js indes.js
+COPY index.js index.js
 COPY package.json package.json
 
 RUN npm install 
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const readLastLine = require("read-last-line");
+const path = require("path")
 
 const fastify = require("fastify")({});
 
 fastify.get("/logs", (request, reply) => {
-  readLastLine
-    .read("./logs.txt", 100)
-    .then(function (lines) {
-      reply.send(lines);
-    })
-    .catch(function (err) {
-      reply.send("error");
-    });
+        const filePath = path.join(__dirname, "log.txt")
+
+        readLastLine
+                .read(filePath, 200)
+                .then(function (lines) {
+                        reply.send(lines);
+                })
+                .catch(function (err) {
+                        reply.send(filePath);
+                });
 });
 
 fastify.listen(3003, "0.0.0.0", (err, address) => {
-  if (err) throw err;
-  console.log(`server listening on ${address}`);
+        if (err) throw err;
+        console.log(`server listening on ${address}`);
 });
 
